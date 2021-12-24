493cd8f267dc0bc531fa33643d9c85a71b5f43ee
Brayan Luna <blun4n@gmail.com>
2021-12-23 19:43:42 -0600

    Move constants to a single file

---
+++ flic_hub_module/constants.js
@@ -0,0 +1,12 @@
+// CONNECTIVITY_STATES
+exports.CONNECTIVITY_STATE_CONNECTED = 'on';
+exports.CONNECTIVITY_STATE_DISCONNECTED = 'off';
+
+// BUTTON_STATES
+exports.BUTTON_STATE_ON = 'on';
+exports.BUTTON_STATE_OFF = 'off';
+
+// ACTIONS
+exports.CLICK_HOLD = 'hold';
+exports.CLICK_SINGLE = 'single';
+exports.CLICK_DOUBLE = 'double';
