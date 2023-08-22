497bac92a59d81669154826c9808b8c7ccd87a15
blunan <blun4n@gmail.com>
2023-08-22 12:37:51 -0600

    Update WARNING_BATTERY_LEVEL to 8
    Took this from the flic app for one of my buttons

--- flic_hub_module/config.js
+++ flic_hub_module/config.js
@@ -1,5 +1,5 @@
 exports.MIN_EVENTS_OFFSET = 600;
 exports.SYNC_TIME = 1 * 60 * 1000;
-exports.WARNING_BATTERY_LEVEL = 5;
+exports.WARNING_BATTERY_LEVEL = 8;
 exports.SERVER_HOST = 'http://192.168.XXX.YYY:8123';
 exports.SERVER_AUTH_TOKEN = 'Your Long-Lived Access Token';
