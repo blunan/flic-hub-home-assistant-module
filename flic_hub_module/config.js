a4c4357a8b0e8f04bbb0f0cefed3fb59113ce5cf
blunan <blun4n@gmail.com>
2026-02-02 22:05:10 -0600

    Implement HTTP custom trust store

--- flic_hub_module/config.js
+++ flic_hub_module/config.js
@@ -3,3 +3,6 @@ exports.SYNC_TIME = 1 * 60 * 1000;
 exports.WARNING_BATTERY_LEVEL = 8;
 exports.SERVER_HOST = 'http://192.168.XXX.YYY:8123';
 exports.SERVER_AUTH_TOKEN = 'Your Long-Lived Access Token';
+exports.USE_CUSTOM_CERTIFICATE = false
+exports.VERIFY_CERTIFICATE = false
+exports.CUSTOM_CERTIFICATES = "-----BEGIN CERTIFICATE-----MIICizCCAfSgAwIBAgIUGu...8gJynKjhWy2vGoDJNx36I+-----END CERTIFICATE-----"
