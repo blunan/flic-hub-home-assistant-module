eb76136927485ab51f3e576990bdb51529a2eb2f
blunan <blun4n@gmail.com>
2026-02-02 22:05:56 -0600

    v1.2.5

--- flic_hub_module/config.js
+++ flic_hub_module/config.js
@@ -1,7 +1,7 @@
 exports.MIN_EVENTS_OFFSET = 600;
 exports.SYNC_TIME = 1 * 60 * 1000;
 exports.WARNING_BATTERY_LEVEL = 8;
-exports.SERVER_HOST = 'http://192.168.XXX.YYY:8123';
+exports.SERVER_HOST = 'https://192.168.XXX.YYY:8123';
 exports.SERVER_AUTH_TOKEN = 'Your Long-Lived Access Token';
 exports.USE_CUSTOM_CERTIFICATE = false
 exports.VERIFY_CERTIFICATE = false
